86d41f4615ff27469c7e9973c3597bcef8581d6f
Ayush Sanj <ayushsanjpro@gmail.com>
2021-04-29 12:24:32 +0530

    add app css and id change

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from 'react';
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <h1>
-        Welcome to Evermemo
+        Evermemo
       </h1>
     </div>
   );
