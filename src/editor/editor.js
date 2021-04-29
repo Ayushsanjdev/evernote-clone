60efde8d3be3e56afd41ec536c0f6abf3f93a4d1
Ayush Sanj <ayushsanjpro@gmail.com>
2021-04-29 16:17:14 +0530

    add imports from editor

---
+++ src/editor/editor.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactQuill from 'react-quill';
+import debounce from '../helper_debouncer';
+import BorderColorIcon from '@material-ui/icons/BorderColor';
+import { withStyles } from '@material-ui/core/styles';
+import  styles from './styles';
